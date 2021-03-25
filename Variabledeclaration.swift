23ad2e29ff6113723acd336f4394bb3bffeaccbe
Arundeepmahajan <58620121+Arundeepmahajan@users.noreply.github.com>
2021-03-25 12:57:42 +0530

    starting today uploading java soon
    Sorry was busy in many different things

---
+++ Variabledeclaration.swift
@@ -0,0 +1,10 @@
+import Swift
+var siteName:String
+siteName="Apple.com"
+/* OR SOMETHING LIKE THIS*/
+var something:String="Google.com"
+print(something)
+print(siteName)
+/*Oh this too*/
+let thisplacename:String="Wikipedia.com"
+print(thisplacename)
